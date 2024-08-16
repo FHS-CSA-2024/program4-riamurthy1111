61ba6b14dfa655f4f5eb78cc80bee732e17c85c1
riamurthy1111 <riamurthy1111@gmail.com>
2024-08-16 09:25:26 -0700

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,10 +1,29 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
-
+public class Program4{
+    public static void main(String[] args) {
+        int Variable1 = 475;
+        int Variable2 = 821;
+        int Variable3 = 369;
+        int Variable4 = 562;
+        int Variable5 = 0;
+        double Variable6 = 0;
+        Variable5 = Variable1 + Variable2 + Variable3 + Variable4;
+        Variable6 = Variable5 / 4.0;
+        
+        System.out.println ("The sum of the four numbers is " + Variable5);
+        System.out.println();
+        System.out.println("The average of the four numbers " + Variable6);
+        
+        
+        
+    }
+}
 
 
 //Paste console output below:
 /*
+The sum of the four numbers is 2227
 
-
+The average of the four numbers 556.75
 */
